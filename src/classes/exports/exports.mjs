6ab812efe8a70f91f59e8b3e540024571d7b06d5
yashitjain <jainyashit11@gmail.com>
2024-06-13 00:36:33 +0530

    learn exports learn components props and prototypes and defaultprops

---
+++ src/classes/exports/exports.mjs
@@ -0,0 +1,6 @@
+const a = 1;
+const b = 2;
+const c = 3;
+
+export default a;
+export {b,c};
---
+++ src/classes/exports/exports2.mjs
@@ -0,0 +1,3 @@
+import anything,{b,c} from './exports.mjs'
+console.log(anything);          //a is being exported by default therefore it is anything==>a
+console.log(b,c);               //can't do same for b and c
