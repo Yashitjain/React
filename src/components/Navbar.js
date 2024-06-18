c056874f3c16eb655c6072933ed7bddd34c303cd
Yashitjain <72515029+Yashitjain@users.noreply.github.com>
2024-06-18 17:52:43 +0000

    changed home href in Navbar from '/' to '#' so that 404 error doesn't comes up

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,7 +11,7 @@ export default function Navbar(props) {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <a className="nav-a active" aria-current="page" href="/"> <b>Home</b></a>
+                <a className="nav-a active" aria-current="page" href="#"> <b>Home</b></a>
               </li>
             </ul>
 
